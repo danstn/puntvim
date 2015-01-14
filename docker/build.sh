8e7a7b1556a35b84b7401a140a72400d98bf5d97
Daniel Stankevich <standeo@gmail.com>
2015-01-15 01:16:39 +1100

    Container baking updates

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Package artifacts
+./package.sh
+
+# Bake a container
 CONTAINER_TAG="dzotokan/vim"
-
 docker build -t $CONTAINER_TAG .
-
 docker images $CONTAINER_TAG
+
+# Post build cleanup
+./clean.sh
---
+++ docker/package.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Copy vimrc that will live inside the container
+cp ../vimrc-basic ./vimrc-docker
