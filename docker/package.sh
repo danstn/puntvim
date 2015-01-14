5b5efcb3940791aaf729edd866afda1cf8fc6d5c
Daniel Stankevich <standeo@gmail.com>
2015-01-15 03:51:44 +1100

    _Disabling_ not ready plugins

--- docker/package.sh
+++ docker/package.sh
@@ -9,3 +9,8 @@ cp -r ../colors     ./$ARTIFACT_DIR/colors
 cp -r ../fonts      ./$ARTIFACT_DIR/fonts
 cp -r ../syntax     ./$ARTIFACT_DIR/syntax
 cp -r ../bundle     ./$ARTIFACT_DIR/bundle
+
+# @TODO: Fix this dirty hack re: ultisnips mapping bug
+rm -rf ./$ARTIFACT_DIR/bundle/YouCompleteMe
+rm -rf ./$ARTIFACT_DIR/bundle/ultisnips
+rm -rf ./$ARTIFACT_DIR/bundle/neocomplete.vim
