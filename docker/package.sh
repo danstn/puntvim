5a3e6bae03c1354a2778c13613c59a35b5be335a
Daniel Stankevich <daniel.stankevich@rea-group.com>
2015-01-15 17:03:35 +1100

    base on busybox

--- docker/package.sh
+++ docker/package.sh
@@ -4,6 +4,12 @@ ARTIFACT_DIR=vim-artifact
 
 # Package vim-artifact
 mkdir -p $ARTIFACT_DIR
+
+wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
+bunzip2 vim-7.4.tar.bz2
+tar -xvf vim-7.4.tar
+cp -r vim74 ./$ARTIFACT_DIR/vim
+
 cp ../vimrc-docker  ./$ARTIFACT_DIR/vimrc-docker
 cp -r ../colors     ./$ARTIFACT_DIR/colors
 cp -r ../fonts      ./$ARTIFACT_DIR/fonts
